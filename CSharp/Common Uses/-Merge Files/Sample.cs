500b35719baa7affe7e891502c40a18a21757ca3
MaxSautin <max.sautinsoft@gmail.com>
2023-09-12 15:43:27 +0400

    Added Merge PDF example.

--- CSharp/Common Uses/-Merge Files/Sample.cs
+++ CSharp/Common Uses/-Merge Files/Sample.cs
@@ -10,25 +10,40 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            // List of source file names.
-            var fileNames = new string[]
-            {
-            @"..\..\..\MergeFile01.pdf",
-            @"..\..\..\MergeFile02.pdf",
-            @"..\..\..\MergeFile03.pdf",
-            @"..\..\..\MergeFile04.pdf"
-            };
+            MergePdf();
+        }
+        /// <summary>
+        /// How to merge PDF documents into a single PDF.
+        /// </summary>
+        /// <remarks>
+        /// Details: https://sautinsoft.com/products/pdf/help/net/developer-guide/merge-pdf-files.php
+        /// </remarks>
+        static void MergePdf()
+        {
+            string[] inpFiles = new string[] {
+                        Path.GetFullPath(@"..\..\..\File1.pdf"),
+                        Path.GetFullPath(@"..\..\..\File2.pdf"),
+                        Path.GetFullPath(@"..\..\..\File3.pdf")};
 
-            using (var document = new PdfDocument())
+            string outFile = Path.GetFullPath(@"Merged.pdf");
+
+            using (var pdf = new PdfDocument())
             {
                 // Merge multiple PDF files into single PDF by loading source documents
                 // and cloning all their pages to destination document.
-                foreach (var fileName in fileNames)
-                    using (var source = PdfDocument.Load(fileName))
-                        document.Pages.Kids.AddClone(source.Pages);
-
-                document.Save("Merge Files.pdf");
+                foreach (var inpFile in inpFiles)
+                {
+                    using (var source = PdfDocument.Load(inpFile))
+                    {
+                        foreach (var page in source.Pages)
+                            pdf.Pages.AddClone(page);
+                    }
+                }
+                pdf.Save(outFile);
             }
+            // Show the result PDF document.
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(outFile) { UseShellExecute = true });
         }
+
     }
 }
