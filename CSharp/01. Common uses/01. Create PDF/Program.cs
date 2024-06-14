38cd869a55e0cdc9e1e591bafe6bdce5a5a698da
unknown <D:\Soft\Email\TheBat\Data\Mail>
2024-06-14 11:17:42 +0400

    New Code Samples

--- CSharp/01. Common uses/01. Create PDF/Program.cs
+++ CSharp/01. Common uses/01. Create PDF/Program.cs
@@ -16,11 +16,15 @@ class Program
         // Apply the key here:
         // PdfDocument.SetLicense("...");
 
-        PdfDocument document = new PdfDocument();
-
-        if (document != null)
+        using (var document = new PdfDocument())
         {
-            Console.WriteLine("File created successfully!");
+            using (var formattedText = new PdfFormattedText())
+            {
+                var page = document.Pages.Add();
+                formattedText.Append("Hello from SautinSoft");
+                page.Content.DrawText(formattedText, new PdfPoint(250, 330));
+            }
+            document.Save("Output.pdf");
         }
     }
 }
