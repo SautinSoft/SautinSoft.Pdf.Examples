3febeb7d0c143ecaa4d63357ef11d771e9d18db1
MaxSautin <max.sautinsoft@gmail.com>
2024-06-24 09:07:57 +0400

    Updated examples.

--- CSharp/01. Common uses/01. Create PDF/Program.cs
+++ CSharp/01. Common uses/01. Create PDF/Program.cs
@@ -2,29 +2,71 @@ using System;
 using SautinSoft.Pdf;
 using System.IO;
 using SautinSoft.Pdf.Content;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 
-class Program
+namespace Sample
 {
-    /// <remarks>
-    /// Details: https://sautinsoft.com/products/pdf/help/net/developer-guide/create-pdf.php
-    /// </remarks>
-    static void Main()
+    class Program
     {
-        // Before starting this example, please get a free 30-day trial key:
-        // https://sautinsoft.com/start-for-free/
-
-        // Apply the key here:
-        // PdfDocument.SetLicense("...");
-
-        using (var document = new PdfDocument())
+        /// <remarks>
+        /// Details: https://sautinsoft.com/products/pdf/help/net/developer-guide/create-pdf.php
+        /// </remarks>
+        static void Main()
         {
-            using (var formattedText = new PdfFormattedText())
+            // Before starting this example, please get a free 30-day trial key:
+            // https://sautinsoft.com/start-for-free/
+
+            // Apply the key here:
+            // PdfDocument.SetLicense("...");
+
+            using (var document = new PdfDocument())
+
             {
                 var page = document.Pages.Add();
-                formattedText.Append("Hello from SautinSoft");
-                page.Content.DrawText(formattedText, new PdfPoint(250, 330));
+
+                var formattedText1 = new PdfFormattedText();
+                var text1 = "Hello World";
+                formattedText1.FontSize = 15;
+                formattedText1.FontFamily = new PdfFontFamily("Calibri");
+                formattedText1.Append(text1);
+                page.Content.DrawText(formattedText1, new PdfPoint(110, 650));
+
+                var formattedText2 = new PdfFormattedText();
+                var text2 = "This message was";
+                formattedText2.FontSize = 16;
+                formattedText2.FontFamily = new PdfFontFamily("Times New Roman");
+                formattedText2.FontStyle = PdfFontStyle.Italic;
+                formattedText2.Color = PdfColor.FromRgb(1, 0, 0);
+                formattedText2.Append(text2);
+                page.Content.DrawText(formattedText2, new PdfPoint(115, 632));
+                
+                var formattedText3 = new PdfFormattedText();
+                var text3 = "created by SautinSoft";
+                formattedText3.FontSize = 22;
+                formattedText3.FontStyle = PdfFontStyle.Italic;
+                formattedText3.FontFamily = new PdfFontFamily("Times New Roman");
+                formattedText3.Color = PdfColor.FromRgb(1, 0, 0);
+                formattedText3.Append(text3);
+                page.Content.DrawText(formattedText3, new PdfPoint(110, 610));
+                
+                var formattedText4 = new PdfFormattedText();
+                var text4 = "component!";
+                formattedText4.FontSize = 22;
+                formattedText4.FontFamily = new PdfFontFamily("Times New Roman");
+                formattedText4.Append(text4);
+                page.Content.DrawText(formattedText4, new PdfPoint(303, 610));
+
+
+
+                document.Save("Output.pdf");
             }
-            document.Save("Output.pdf");
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("Output.pdf") { UseShellExecute = true });
         }
     }
 }
+      
+    
+
+    
