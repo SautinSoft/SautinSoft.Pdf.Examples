4156d1ea9343f780a80edad5b288705aa3d2f50f
MaxSautin <max.sautinsoft@gmail.com>
2024-08-13 14:32:06 +0400

    Updated examples.

--- CSharp/03. Text/06. Find and Replace/Program.cs
+++ CSharp/03. Text/06. Find and Replace/Program.cs
@@ -22,53 +22,25 @@ namespace Sample
             // Apply the key here:
             // PdfDocument.SetLicense("...");
 
-            string inpFile = Path.GetFullPath(@"..\..\..\simple text.pdf");            
+            string inpFile = Path.GetFullPath(@"..\..\..\simple text.pdf");
             string outFile = Path.GetFullPath("Result.pdf");
 
             using (PdfDocument document = PdfDocument.Load(inpFile))
             {
-                // Assume we want to find the word "North"
-                // and replace it to the "South".
-                string textFrom = "North";
-                string textTo = "South";
-
-                // Iterate by all pages
                 foreach (var page in document.Pages)
                 {
-                    // Find the text.
-                    var texts = page.Content.GetText().Find(textFrom);
-
-                    // Get the text coordinates and font from 1st Element;
-                    // Draw the new text.
-                    foreach (var text in texts)
+                    var search = page.Content.GetText().Find("North");
+                    foreach (var text in search)
                     {
-                        foreach (var el in text.Elements)
+                        var element = text.Elements.First();
+                        var font = element.Format.Text.Font;
+                        double size = Math.Min(text.Bounds.Height, font.Size * element.TextTransform.M11);
+
+                        using (var formattedText = new PdfFormattedText() { Font = font, Color = PdfColor.FromRgb(1, 0, 0), FontSize = size })
                         {
-                            // Get the text formatting, coordinates;
-                            // Draw the new text "South".
-                            using (var formattedText = new PdfFormattedText())
-                            {
-                                formattedText.Language = new PdfLanguage("en-US");
-
-                                double fontSize = Math.Min(text.Bounds.Height, el.Format.Text.Font.Size * el.TextTransform.M11);
-
-                                // Case 1: Use a new font, looks different, but contains all characters.
-                                //formattedText.Font = new PdfFont("Helvetica", fontSize);
-
-                                // Case 2: Use the current font, it looks the same, but may contain missing characters.
-                                formattedText.Font = el.Format.Text.Font;
-                                formattedText.FontSize = fontSize;
-
-                                // Set "Red" color
-                                formattedText.Color = PdfColor.FromRgb(1, 0, 0);
-                                formattedText.Append(textTo);
-
-                                page.Content.DrawText(formattedText, new PdfPoint(text.Bounds.Left, text.Bounds.Bottom));
-
-                            }
-                            break;
+                            formattedText.Append("South");
+                            page.Content.DrawText(formattedText, new PdfPoint(text.Bounds.Left, text.Bounds.Bottom));
                         }
-                        // Remove the text.
                         text.Redact();
                     }
                 }
