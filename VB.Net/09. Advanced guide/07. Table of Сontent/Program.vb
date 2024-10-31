cffe1e73276aa28b154f9589d019e42b475355d2
developer@SupportPC <Developer@SupportPC>
2024-10-31 16:35:35 +0400

    New Code Samples

--- VB.Net/09. Advanced guide/07. Table of Сontent/Program.vb
+++ VB.Net/09. Advanced guide/07. Table of Сontent/Program.vb
@@ -1,5 +1,3 @@
-Option Infer On
-
 Imports System
 Imports System.IO
 Imports System.Reflection.Metadata
@@ -10,107 +8,110 @@ Imports SautinSoft.Pdf.Annotations
 Imports SautinSoft.Pdf.Content
 
 Namespace Sample
-	Friend Class Sample
-		''' <summary>
-		''' Merge PDF files and create TOC.
-		''' </summary>
-		''' <remarks>
-		''' Details: https://sautinsoft.com/products/pdf/help/net/developer-guide/table-of-content.php
-		''' </remarks>
-		Shared Sub Main(ByVal args() As String)
-			' Before starting this example, please get a free 100-day trial key:
-			' https://sautinsoft.com/start-for-free/
+    Class Sample
+        ''' <summary>
+        ''' Merge PDF files and create TOC.
+        ''' </summary>
+        ''' <remarks>
+        ''' Details: https://sautinsoft.com/products/pdf/help/net/developer-guide/table-of-content.php
+        ''' </remarks>
+        Shared Sub Main(args As String())
+            ' Before starting this example, please get a free 100-day trial key:
+            ' https://sautinsoft.com/start-for-free/
 
-			' Apply the key here:
-			' PdfDocument.SetLicense("...");
+            ' Apply the key here:
+            ' PdfDocument.SetLicense("...")
 
-			Dim inpFiles() As String = { Path.GetFullPath("..\..\..\Simple Text.pdf"), Path.GetFullPath("..\..\..\Potato Beetle.pdf"), Path.GetFullPath("..\..\..\Text and Graphics.pdf")}
+            Dim inpFiles As String() = {
+                Path.GetFullPath("..\..\..\Simple Text.pdf"),
+                Path.GetFullPath("..\..\..\Potato Beetle.pdf"),
+                Path.GetFullPath("..\..\..\Text and Graphics.pdf")
+            }
 
-			Dim outFile As String = Path.GetFullPath("Merged.pdf")
-			Dim tocEntries = New List(Of (Title As String, PagesCount As Integer))()
-			' Create a new PDF document.
-			Using pdf = New PdfDocument()
-				' Merge multiple PDF documents the new single PDF.
-				For Each inpFile In inpFiles
-					Using source = PdfDocument.Load(inpFile)
-						pdf.Pages.Kids.AddClone(source.Pages)
-						tocEntries.Add((Path.GetFileNameWithoutExtension(inpFile), source.Pages.Count))
-					End Using
-				Next inpFile
+            Dim outFile As String = Path.GetFullPath("Merged.pdf")
+            Dim tocEntries As New List(Of (Title As String, PagesCount As Integer))
 
-				Dim pagesCount As Integer
-				Dim tocPagesCount As Integer
+            ' Create a new PDF document.
+            Using pdf As New PdfDocument()
+                ' Merge multiple PDF documents the new single PDF.
+                For Each inpFile In inpFiles
+                    Using source As PdfDocument = PdfDocument.Load(inpFile)
+                        pdf.Pages.Kids.AddClone(source.Pages)
+                        tocEntries.Add((Path.GetFileNameWithoutExtension(inpFile), source.Pages.Count))
+                    End Using
+                Next
 
-				' Create PDF with Table of Contents.
-				Using tocDocument = PdfDocument.Load(CreatePdfWithToc(tocEntries))
-					pagesCount = tocDocument.Pages.Count
-					tocPagesCount = pagesCount - tocEntries.Sum(Function(entry) entry.PagesCount)
+                Dim pagesCount As Integer
+                Dim tocPagesCount As Integer
 
-					' Remove empty (placeholder) pages.
-					For i As Integer = pagesCount - 1 To tocPagesCount Step -1
-						tocDocument.Pages.RemoveAt(i)
-					Next i
+                ' Create PDF with Table of Contents.
+                Using tocDocument As PdfDocument = PdfDocument.Load(CreatePdfWithToc(tocEntries))
+                    pagesCount = tocDocument.Pages.Count
+                    tocPagesCount = pagesCount - tocEntries.Sum(Function(entry) entry.PagesCount)
 
-					' Insert TOC pages.
-					pdf.Pages.Kids.InsertClone(0, tocDocument.Pages)
-				End Using
+                    ' Remove empty (placeholder) pages.
+                    For i As Integer = pagesCount - 1 To tocPagesCount Step -1
+                        tocDocument.Pages.RemoveAt(i)
+                    Next
 
-				Dim entryIndex As Integer = 0
-				Dim entryPageIndex As Integer = tocPagesCount
+                    ' Insert TOC pages.
+                    pdf.Pages.Kids.InsertClone(0, tocDocument.Pages)
+                End Using
 
-				' Update TOC links and outlines so that they point to adequate pages instead of placeholder pages.
-				For i As Integer = 0 To tocPagesCount - 1
-					For Each annotation In pdf.Pages(i).Annotations.OfType(Of PdfLinkAnnotation)()
-						Dim entryPage = pdf.Pages(entryPageIndex)
-						annotation.SetDestination(entryPage, PdfDestinationViewType.FitPage)
+                Dim entryIndex As Integer = 0
+                Dim entryPageIndex As Integer = tocPagesCount
 
-						entryPageIndex += tocEntries(entryIndex).PagesCount
-						entryIndex += 1
-					Next annotation
-				Next i
+                ' Update TOC links and outlines so that they point to adequate pages instead of placeholder pages.
+                For i As Integer = 0 To tocPagesCount - 1
+                    For Each annotation In pdf.Pages(i).Annotations.OfType(Of PdfLinkAnnotation)()
+                        Dim entryPage As PdfPage = pdf.Pages(entryPageIndex)
+                        annotation.SetDestination(entryPage, PdfDestinationViewType.FitPage)
 
-				pdf.Save(outFile)
-			End Using
-			' Show the result.
-			System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(outFile) With {.UseShellExecute = True})
-		End Sub
+                        entryPageIndex += tocEntries(entryIndex).PagesCount
+                        entryIndex += 1
+                    Next
+                Next
 
-		Private Shared Function CreatePdfWithToc(ByVal tocEntries As List(Of (Title As String, PagesCount As Integer))) As Stream
-			' Create new document.
-			Dim document = New DocumentCore()
-			Dim section As New Section(document)
-			document.Sections.Add(section)
+                pdf.Save(outFile)
+            End Using
 
-			' Add Table of Content.
-			Dim toc = New TableOfEntries(document, FieldType.TOC)
-			section.Blocks.Add(toc)
+            ' Show the result.
+            System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(outFile) With {.UseShellExecute = True})
+        End Sub
 
-			' Create heading style.
-			Dim heading1Style = CType(document.Styles.GetOrAdd(StyleTemplateType.Heading1), ParagraphStyle)
-			heading1Style.ParagraphFormat.PageBreakBefore = True
+        Shared Function CreatePdfWithToc(tocEntries As List(Of (Title As String, PagesCount As Integer))) As Stream
+            ' Create new document.
+            Dim document As New DocumentCore()
+            Dim section As New Section(document)
+            document.Sections.Add(section)
 
-			' Add heading paragraphs and empty (placeholder) pages.
-			For Each tocEntry In tocEntries
-				section.Blocks.Add(New Paragraph(document, tocEntry.Title) With {
-					.ParagraphFormat = { Style = heading1Style }
-				})
+            ' Add Table of Content.
+            Dim toc As New TableOfEntries(document, FieldType.TOC)
+            section.Blocks.Add(toc)
 
-				For i As Integer = 0 To tocEntry.PagesCount - 1
-					section.Blocks.Add(New Paragraph(document, New SpecialCharacter(document, SpecialCharacterType.PageBreak)))
-				Next i
-			Next tocEntry
+            ' Create heading style.
+            Dim heading1Style As ParagraphStyle = CType(document.Styles.GetOrAdd(StyleTemplateType.Heading1), ParagraphStyle)
+            heading1Style.ParagraphFormat.PageBreakBefore = True
 
-			' Remove last extra-added empty page.
-			section.Blocks.RemoveAt(section.Blocks.Count - 1)
+            ' Add headings and placeholder pages.
+            For Each entry In tocEntries
+                Dim heading As New Paragraph(document)
+                heading.ParagraphFormat.Style = heading1Style
+                heading.Content.Start.Insert(entry.Title)
+                section.Blocks.Add(heading)
 
-			' When updating TOC element, an entry is created for each paragraph that has heading style.
-			' The entries have the correct page numbers because of the added placeholder pages.
-			toc.Update()
+                For i As Integer = 1 To entry.PagesCount
+                    section.Blocks.Add(New Paragraph(document))
+                Next
+            Next
 
-			' Save document as PDF.
-			Dim pdfStream = New MemoryStream()
-			document.Save(pdfStream, New SautinSoft.Document.PdfSaveOptions())
-			Return pdfStream
-		End Function
-	End Class
+            ' Save the document to a memory stream.
+            Dim stream As New MemoryStream()
+            document.Save(stream, New SautinSoft.Document.PdfSaveOptions())
+            stream.Position = 0
+            Return stream
+        End Function
+    End Class
 End Namespace
+
+
